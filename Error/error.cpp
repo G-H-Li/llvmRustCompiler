b6da79058770a39554313b1c4430259a92cb5854
GH-Li <2215228145@qq.com>
2021-01-03 13:58:25 +0800

    add error module

---
+++ Error/error.cpp
@@ -0,0 +1,21 @@
+#include "error.h"
+
+void llvmRustCompiler::errorToken(const std::string& msg)
+{
+	std::cerr << "Token Error:" << msg << std::endl;
+	//Scanner::setErrorFlag(true);
+}
+
+void llvmRustCompiler::errorToken(const token& token, const std::string& msg)
+{
+}
+
+void llvmRustCompiler::errorSyntax(const std::string& msg)
+{
+	std::cerr << "Syntax Error:" << msg << std::endl;
+}
+
+void llvmRustCompiler::errorGenerator(const std::string& msg)
+{
+	std::cerr << "Generator Error:" << msg << std::endl;
+}
---
+++ Error/error.h
@@ -0,0 +1,24 @@
+#pragma once
+/*
+* Author: 李国豪
+* Date:2021/1/3
+* description:定义error相关函数
+* latest date:2021/1/3
+*/
+
+#include <string>
+#include "..\Lexer\token.h"
+
+namespace llvmRustCompiler
+{
+	// 使用字符串进行错误词法错误输出
+	extern void errorToken(const std::string& msg);
+	// 使用token进行错误词法错误输出
+	extern void errorToken(const token& token, const std::string& msg);
+	//使用字符串进行错误语法错误输出
+	extern void errorSyntax(const std::string& msg);
+	
+	//使用字符串进行代码生成阶段错误输出
+	extern void errorGenerator(const std::string& msg);
+
+}
