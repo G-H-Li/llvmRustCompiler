e45c7299a2dd7bfc9c548c0aabd6603252eaf08e
清锋 张 <1833072263@qq.com>
2021-01-03 20:10:36 +0800

    已经修复打开源文件错误时lexer还能运行的错误，准备优化读取到错误单词时仍然采用正确分割的bug前先提交一下

--- llvmRustCompiler.cpp
+++ llvmRustCompiler.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int main()
 {
     string fileAddress = "D:\\CodeFile\\lexerTest.txt";
-    llvmRustCompiler::Scanner scanner("fileAddress");
-    scanner.getNextToken();
-
+    llvmRustCompiler::Scanner scanner(fileAddress);
+    scanner.getNextToken();//开始扫描
+    //注意使用Scanner::getErrorFlag()、scanner.getToken().getTokenType()来指示lexer的运行
     while (!llvmRustCompiler::Scanner::getErrorFlag()) {
         scanner.getToken().dump();
         scanner.getNextToken();
