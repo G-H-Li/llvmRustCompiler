35638ddfe8b8fe47126acfaebd941bdd368e917a
Sakai Izumi <izumisakai@aliyun.com>
2021-01-05 13:01:37 +0800

    修改部分函数定义

--- Parser/parser.h
+++ Parser/parser.h
@@ -28,6 +28,8 @@ namespace llvmRustCompiler {
 		std::unique_ptr<ExprAST> ParseParenExpr();
 		std::unique_ptr<ExprAST> ParseIdentifierExpr();
 		std::unique_ptr<ExprAST> ParseLet();
+		std::unique_ptr<ExprAST> ParseIfExpr();
+		std::unique_ptr<ExprAST> ParseForExpr();
 		std::unique_ptr<ExprAST> ParsePrimary();
 		std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
 			std::unique_ptr<ExprAST> LHS);
@@ -35,7 +37,6 @@ namespace llvmRustCompiler {
 		std::unique_ptr<PrototypeAST> ParsePrototype();
 		std::unique_ptr<FunctionAST> ParseDefinition();
 		std::unique_ptr<FunctionAST> ParseTopLevelExpr();
-		std::unique_ptr<ExprAST> ParseIfExpr();
 
 
 	private:
