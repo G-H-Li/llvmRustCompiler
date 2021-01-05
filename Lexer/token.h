84a06fa21d39c43f632f5fc01624f25f48fba0a6
清锋 张 <1833072263@qq.com>
2021-01-05 12:53:37 +0800

    加上了遍历符 .. ，读取括号的调试有大改，下次提交

--- Lexer/token.h
+++ Lexer/token.h
@@ -100,6 +100,7 @@ namespace llvmRustCompiler
         PERIOD,            // .
         SEMICOLON,         // ;
         COLON,             // :
+        TRAVERSE,          // ..
 
         PLUS,              // +
         MINUS,             // -
