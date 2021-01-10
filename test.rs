d160ba07cd2503f128c67b82def84f2f5f9ea989
GH-Li <2215228145@qq.com>
2021-01-10 19:57:27 +0800

    completed

--- test.rs
+++ test.rs
@@ -1,11 +1,11 @@
 //test
-fn add(x:i32, y:i32) -> i32 { return x+y; };
+fn add(x:i32, y:i32) -> i32 { return x+y; }
 fn wmain(){
     let mut b:i32 = 2 << 2;
     while b > 8{
         b += add(2,1);
-    };
-};
+    }
+}
 
 fn main(){
     let mut n:i32 = 5 + 3; 
@@ -14,8 +14,8 @@ fn main(){
         n = n/2;
     }else{
         n = 5;
-    };
-};
+    }
+}
 
 fn fmain(){
     let mut a:f32 = 5.3 - 2.3;
@@ -24,5 +24,5 @@ fn fmain(){
         a = a* 2.5;
     }else{
         a = 4.5;
-    };
+    }
 }
